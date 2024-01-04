0652d445f86926126a91e4310096cb1118d7b75e
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-01-04 18:44:09 +0530

    add clustering

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const app = require("./app");
 const connectDatabase = require("./config/database");
-
-
 connectDatabase();
-
+const cluster = require("cluster");
+const totalCpus = require("os").cpus();
+cluster.schedulingPolicy = cluster.SCHED_RR;
 const port = process.env.PORT;
-app.listen(port, () => {
-  console.log("app is listening on ", port);
-});
 
+
+if (cluster.isMaster) {
+  totalCpus.forEach(async (node) => {
+    await cluster.fork();
+  });
+  cluster.on("exit", async (worker, code, signal) => {
+    await cluster.fork();
+  });
+} else {
+  app.listen(port, () => {
+    console.log(`Process ${process.pid} is online on port number ${port}`);
+  });
+}
