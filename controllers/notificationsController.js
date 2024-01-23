98086905eb2f96670db33cc63a07fd26e0cfdc27
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-01-23 11:41:11 +0530

    cahnge in notification route

--- controllers/notificationsController.js
+++ controllers/notificationsController.js
@@ -2,11 +2,11 @@ const notificationsModel = require("../models/notificationsModel");
 const catchAsyncError = require("../utils/catchAsyncError");
 
 exports.getNotification = catchAsyncError(async (req, res, next) => {
-    const notification = await notificationsModel.find({owner:req.userId}).populate("notifications").lean();
+    const notification = await notificationsModel.findOne({owner:req.userId}).populate("notifications").lean();
     res.status(200).send({
       status: 200,
       success: true,
       
-      data: notification.notifications,
+      data: notification,
     });
   });
