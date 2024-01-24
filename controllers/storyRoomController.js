5974f813a28a411622415488dde99302bbbf6b90
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-01-24 11:58:59 +0530

    changes in route

--- controllers/storyRoomController.js
+++ controllers/storyRoomController.js
@@ -4,6 +4,7 @@ const catchAsyncError = require("../utils/catchAsyncError");
 const invitationsModel = require("../models/invitationModel");
 const ErrorHandler = require("../utils/errorHandler");
 const { sendInvitationEmail } = require("../utils/email");
+// const genreModel = require("../models/genreModel");
 
 exports.createRoom = catchAsyncError(async (req, res, next) => {
   const {
@@ -15,9 +16,7 @@ exports.createRoom = catchAsyncError(async (req, res, next) => {
     userInvitations,
   } = req.body;
 
-  if (participants.length < 1) {
-    return next(new ErrorHandler("Please add Atleast One participants", 400));
-  }
+  
   let acceptedInvitation = [req.userId];
 
   const room = await storyRoomModel.create({
@@ -53,7 +52,7 @@ exports.createRoom = catchAsyncError(async (req, res, next) => {
     };
     await sendInvitationEmail(options);
   }
-
+  // const genre = await genreModel.findOne({genre:theme});
   const populatedRoom = await storyRoomModel
     .findById(room._id)
     .populate("host", "userName profileUrl")
@@ -61,7 +60,7 @@ exports.createRoom = catchAsyncError(async (req, res, next) => {
     .lean();
 
   delete populatedRoom.chats;
-
+  // populatedRoom.colour = genre.colour;
   res.status(201).send({
     status: 201,
     success: true,
@@ -74,12 +73,13 @@ exports.getRoomDetails = catchAsyncError(async (req, res, next) => {
   const { roomId } = req.params;
   const roomDetails = await storyRoomModel
     .findById(roomId)
-    .populate("host", "name email")
+    .populate("host", "userName profileUrl")
     .populate("participants._id", "userName profileUrl")
     .lean();
 
   delete roomDetails.chats;
-
+  // const genre = await genreModel.findOne({genre:roomDetails.genre});
+  // roomDetails.colour = genre.colour
   res.status(200).send({
     status: 200,
     success: true,
@@ -123,6 +123,7 @@ exports.getMyStories = catchAsyncError(async (req, res, next) => {
   // console.log(userId);
   const rooms = await storyRoomModel
     .find()
+    .populate("host", "userName profileUrl")
     .populate("participants._id", "userName profileUrl")
     .lean();
   const arr = [];
@@ -186,6 +187,7 @@ exports.getActiveStories = catchAsyncError(async (req, res, next) => {
         },
       ],
     })
+    .populate("host", "userName profileUrl")
     .populate("participants._id", "userName profileUrl")
     .lean();
   for (let data of rooms) {
@@ -227,6 +229,7 @@ exports.getUpcomingStories = catchAsyncError(async (req, res, next) => {
         },
       ],
     })
+    .populate("host", "userName profileUrl")
     .populate("participants._id", "userName profileUrl")
     .lean();
   for (let data of rooms) {
@@ -268,6 +271,7 @@ exports.getCompletedStories = catchAsyncError(async (req, res, next) => {
         },
       ],
     })
+    .populate("host", "userName profileUrl")
     .populate("participants._id", "userName profileUrl")
     .lean();
 
