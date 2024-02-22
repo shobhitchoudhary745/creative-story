391163037bacca0adc1ae94cb24899e695074409
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-02-22 09:51:30 +0530

    changes

--- controllers/updateController.js
+++ controllers/updateController.js
@@ -1,5 +1,6 @@
 const updateModel = require("../models/updateModel");
 const catchAsyncError = require("../utils/catchAsyncError");
+const ErrorHandler = require("../utils/errorHandler");
 
 exports.getUpdates = catchAsyncError(async (req, res, next) => {
   const update = await updateModel.findOne({ owner: req.userId }).lean();
@@ -7,5 +8,30 @@ exports.getUpdates = catchAsyncError(async (req, res, next) => {
     status: 200,
     success: true,
     data: update,
+  });
+});
+
+exports.viewUpdates = catchAsyncError(async (req, res, next) => {
+  const update = await updateModel.findOne({ owner: req.userId });
+  if(!update) return next(new ErrorHandler("model not found",404))
+  update.count = 0;
+  await update.save();
+  res.status(200).send({
+    status: 200,
+    success: true,
+    message: "Updates viewed",
+  });
+});
+
+exports.removeUpdates = catchAsyncError(async (req, res, next) => {
+  const update = await updateModel.findOne({ owner: req.userId });
+  if(!update) return next(new ErrorHandler("model not found",404))
+  const id = req.params.id;
+  update.updates = update.updates.filter((data) => data._id != id);
+  await update.save();
+  res.status(200).send({
+    status: 200,
+    success: true,
+    message: "Update Removed",
   });
 });
--- routes/updateRoutes.js
+++ routes/updateRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const { auth } = require("../middlewares/auth");
-const { getUpdates } = require("../controllers/updateController");
+const {
+  getUpdates,
+  viewUpdates,
+  removeUpdates,
+} = require("../controllers/updateController");
 
 const router = express.Router();
 
 router.get("/getUpdates", auth, getUpdates);
+router.patch("/viewUpdates", auth, viewUpdates);
+router.patch("/removeUpdates/:id", auth, removeUpdates);
 
 module.exports = router;
